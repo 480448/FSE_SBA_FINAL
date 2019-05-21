c4bc8af53e9986d3cfdf76c4497c2ba1cb017f58
480448 <sayon.roy05@gmail.com>
2019-05-21 08:08:34 -0700

    Removed bin files

--- SourceCode/server/ProjectManager/ProjectManager.Tests/bin/Debug/coverage.bat
+++
@@ -1,2 +0,0 @@
-
-C:\Users\Administrator\Desktop\Sayon_480448_SBA_Project_Manager\SourceCode\server\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\Users\Administrator\Desktop\Sayon_480448_SBA_Project_Manager\SourceCode\server\ProjectManager\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll

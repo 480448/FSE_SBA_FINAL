0c21d0284dd1c9582038a571506b04b3033fef94
480448 <sayon.roy05@gmail.com>
2019-05-21 04:00:35 -0700

    Pushing FSE project to GIT

---
+++ SourceCode/server/ProjectManager/ProjectManager.Tests/coverage.bat
@@ -0,0 +1,2 @@
+
+C:\Users\Administrator\Desktop\Sayon_480448_SBA_Project_Manager\SourceCode\server\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\Users\Administrator\Desktop\Sayon_480448_SBA_Project_Manager\SourceCode\server\ProjectManager\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll

a23223bee13ab409a3e96c4f70b986fc30ab959b
480448 <sayon.roy05@gmail.com>
2019-05-22 07:36:03 -0700

    Code coverage report

--- ProjectManagerSln/Server_App/ProjectManager/ProjectManager.Tests/coverage.bat
+++ ProjectManagerSln/Server_App/ProjectManager/ProjectManager.Tests/coverage.bat
@@ -1,2 +1,2 @@
 
-C:\PritamSaha_863276\FSE_SBA\SourceCode\server\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\PritamSaha_863276\FSE_SBA\SourceCode\server\ProjectManager\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
+C:\Users\Administrator\Desktop\Sayon_480448_SBA_Project_Manager\ProjectManagerSln\Server_App\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\Users\Administrator\Desktop\Sayon_480448_SBA_Project_Manager\ProjectManagerSln\Server_App\ProjectManager\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
